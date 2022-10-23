f2c28faac2bcf3e822c52a458b1720d407863766
nilsonamaral <nilsonamaral7@gmail.com>
2022-10-23 01:33:11 -0300

    update js

--- JS/main.js
+++ JS/main.js
@@ -6,14 +6,57 @@ function criaCalculadora() {
             this.cliqueBotoes();
         },
 
+        clearDisplay() {
+            this.display.value = '';
+        },
+
+        apagaUm() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        realizaConta() {
+            let conta = this.display.value;
+
+            try {
+                conta = eval(conta);
+
+                if(!conta) {
+                    alert('Conta Inválida');
+                    return;
+                }
+            }
+               catch(e) {
+
+                }
+        },
+
         cliqueBotoes() {
-            document.addEventListener('click', function(e) {
+            document.addEventListener('click', e => {
                 const el = e.target;
 
                 if(el.classList.contains('btn-num')) {
-                    this.btnParaDisplay();
+                    this.btnParaDisplay(el.innerText);
                 }
-            }) ;
+
+                if(el.classList.contains('btn-clear')){
+                    this.clearDisplay();
+                }
+
+                if(el.classList.contains('btn-del')) {
+                    this.apagaUm();
+                }
+
+                if(el.classList.contains('btn-eq')) {
+                    this.realizaConta();
+                }
+            });
         },
+
+        btnParaDisplay(valor) {
+            this.display.value += valor;
+        }
     };
 }
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
